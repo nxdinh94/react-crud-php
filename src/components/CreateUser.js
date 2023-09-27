8036d20e13f029b42f7a469874b326f8599ab7f8
nxdinh11 <115705231+nxdinh11@users.noreply.github.com>
2023-09-27 14:41:58 +0700

    create db & design form

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,6 +1,57 @@
+import { useState } from "react";
+import axios from "axios";
+
 function CreateUser() {
+
+    const [inputs, setInputs] = useState();
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        console.log(name);
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+
+    }
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(inputs);
+    }
+
     return ( 
-        <h1>Create User</h1>
+        <div>
+            <h1>Create User</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing='10' >
+                    <tbody>
+                        <tr>
+                            <th><label>Name: </label></th>
+                            <td><input 
+                                    type="text" 
+                                    name="name"
+                                    onChange={handleChange}/></td>
+                        </tr>
+                        <tr>
+                            <th><label>Email: </label></th>
+                            <td><input 
+                                    type="text" 
+                                    name="email"
+                                    onChange={handleChange}/></td>
+                        </tr>
+                        <tr>
+                            <th><label>Mobile: </label></th>
+                            <td><input 
+                                    type="text" 
+                                    name="mobile"
+                                    onChange={handleChange}/></td>
+                        </tr>
+                        <tr>
+                            <td align="right"><button>Save</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+            </form>
+        </div>
      );
 }
 
